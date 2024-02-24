755ad372e5efdd1858d49e73e6e1e83eb48ef828
Hrushikesh <khandalehrushikesh@gmail.com>
2024-02-24 17:37:09 +0530

    Implemented the input change handler

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import React, { useState } from "react";
 function App() {
   const [inputs, setInputs] = useState(["", "", "", "", "", "", ""]);
 
+  const handleInputChange = (index, value) => {
+    const inputsClone = [...inputs];
+    inputsClone[index] = value;
+    setInputs(inputsClone);
+  };
+
   return (
     <div>
     <h1>Drag & Drop Fields</h1>
