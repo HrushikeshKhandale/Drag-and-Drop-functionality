70c6becc8afd168cbc99e0ed163aca4b6c8f9f77
Hrushikesh <khandalehrushikesh@gmail.com>
2024-02-24 17:39:43 +0530

    Implemented local storage functionality

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
-// App.js
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
   const [inputs, setInputs] = useState(["", "", "", "", "", "", ""]);
+
+  useEffect(() => {
+    const storedInputs = JSON.parse(localStorage.getItem("inputs"));
+
+    if (storedInputs && storedInputs.length === inputs.length) {
+      setInputs(storedInputs);
+    }
+  }, []);
 
   const handleInputChange = (index, value) => {
     const inputsClone = [...inputs];
     inputsClone[index] = value;
     setInputs(inputsClone);
+    localStorage.setItem("inputs", JSON.stringify(inputsClone));
+  };
+
+  const handleClear = () => {
+    localStorage.removeItem("inputs");
+    setInputs(["", "", "", "", "", "", ""]);
   };
 
   return (
-    <div>
-    <h1>Drag & Drop Fields</h1>
-    {inputs.map((input, index) => (
-      <input
-        key={index}
-        type="text"
-        value={input}
-        onChange={(e) => handleInputChange(index, e.target.value)}
-      />
-    ))}
-    <button onClick={() => setInputs(["", "", "", "", "", "", ""])}>Clear</button>
-  </div>
+    <div className="container">
+      <h1>Drag & Drop Fields</h1>
+      {inputs.map((input, index) => (
+        <input
+          key={index}
+          type="text"
+          value={input}
+          onChange={(e) => handleInputChange(index, e.target.value)}
+        />
+      ))}
+      <button onClick={handleClear}>Clear</button>
+    </div>
   );
 }
 
